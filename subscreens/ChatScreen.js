cd11f4d3973a23dd60c9de467486ea17b2e646c5
javier2409 <javiferreyra97@gmail.com>
2020-04-28 05:47:08 -0300

    agrego que se vean los mensajes y mejoro ui

--- subscreens/ChatScreen.js
+++ subscreens/ChatScreen.js
@@ -1,7 +1,22 @@
 import { useTheme } from '@react-navigation/native';
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, TouchableOpacity, TextInput } from 'react-native';
 import { Avatar, Icon, Input, Text } from 'react-native-elements';
+
+const messages=[
+    {
+        name: 'Franco',
+        msg: 'Hola, todo bien?'
+    },
+    {
+        name: 'Javier',
+        msg: 'Holaaa todo bien y vos?'
+    },
+    {
+        name: 'Franco',
+        msg: 'Viste esta nueva app Chotuve? Dicen que esta buenisima'
+    },
+];
 
 export default function ChatScreen({route, navigation}){
     const {colors} = useTheme();
@@ -9,30 +24,51 @@ export default function ChatScreen({route, navigation}){
     navigation.setOptions({
         headerTitle: () => {
             return (
-                <View style={styles.headerview}>
+                <TouchableOpacity style={styles.headerview}>
                     <Avatar source={{uri: avatar_url}} rounded/>
-                    <Text style={{...styles.headertitle, ...{color: colors.title}}}>{name}</Text>
-                </View>
+                    <Text style={{...styles.headertitle, ...{color: colors.highlight}}}>{name}</Text>
+                </TouchableOpacity>
             );
         }
     });
     return (
         <View style={styles.screenview}>
-            <FlatList/>
-            <View style={{...styles.messagebar, ...{backgroundColor: colors.primary}}}>
-                <View style={{flex: 1}}>
-                    <Input
+            <FlatList
+                data={messages}
+                renderItem={({item}) => {
+                    return (
+                        <View style={{
+                            alignSelf: 
+                                (item.name === name)
+                                ? 'flex-start'
+                                : 'flex-end',
+                            padding: 10,
+                            margin: 10,
+                            backgroundColor: colors.lighterbackground,
+                            maxWidth: '50%'
+                        }}>
+                            <Text style={{color: colors.text}}>
+                                {item.msg}
+                            </Text>
+                        </View>
+                    );
+                }}
+            />
+            <View style={{...styles.messagebar}}>
+                <View style={{...styles.inputview, ...{backgroundColor: colors.primary}}}>
+                    <TextInput
                         placeholder= 'Mensaje' 
                         selectionColor= {colors.text}
-                        inputStyle={{
+                        style={{
                             color: colors.text
                         }}
                     />
                 </View>
                 <Icon 
                     name='send' 
-                    color={colors.background} 
+                    color={colors.primary} 
                     raised
+                    reverse
                 />
             </View>
         </View>
@@ -46,17 +82,24 @@ const styles = StyleSheet.create({
     },
     headertitle: {
         color: 'white', 
-        fontSize: 20, 
+        fontSize: 22, 
         fontWeight: 'bold',
         marginLeft: 10
     },
     messagebar: {
         flexDirection: 'row',
         alignItems: 'center',
-        paddingLeft: 10
+        paddingLeft: 10,
+        backgroundColor: '#00000000'
     },
     screenview: {
         justifyContent: 'flex-end',
-        flex: 1
+        flex: 1,
+    },
+    inputview: {
+        flex: 1,
+        borderRadius: 30,
+        padding: 12,
+        paddingLeft: 17
     }
 });
