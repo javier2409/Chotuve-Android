f772a0e0f4f946dec59f6da28d6b11e96b54df2d
Javier <javiferreyra97@gmail.com>
2020-05-11 06:59:02 -0300

    uso AuthContext en LoginScreen

--- login/LoginScreen.js
+++ login/LoginScreen.js
@@ -1,12 +1,13 @@
 import { useTheme } from '@react-navigation/native';
-import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Divider, Icon, Input } from 'react-native-elements';
-import * as ImagePicker from 'expo-image-picker';
 import { Button } from 'react-native';
-import { Video } from 'expo-av';
+import { AuthContext } from '../App';
 
 export default function LoginScreen(){
+
+    const [userData, setUserData] = useContext(AuthContext);
+
     return (
         <View>
             <Text>Hola!</Text>
