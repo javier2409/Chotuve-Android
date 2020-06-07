f5048b1b58f812a1468b96983f7f2a90f255dfc1
Javier <javiferreyra97@gmail.com>
2020-06-07 10:53:52 -0300

    fetch solicitudes

--- tabs/Notifications.js
+++ tabs/Notifications.js
@@ -1,10 +1,35 @@
-import React from "react";
-import {View} from "react-native";
+import React, {useCallback, useContext, useState} from "react";
+import {ScrollView, View} from "react-native";
+import {Text} from "react-native-elements";
+import {ThemeContext} from "../Styles";
+import {AuthContext} from "../login/AuthContext";
+import {useFocusEffect} from "@react-navigation/native";
 
 export default function Notifications(){
+    const {styles} = useContext(ThemeContext);
+    const [user, server] = useContext(AuthContext);
+    const [friendRequests, setFriendRequests] = useState([]);
+
+    function fetchFriendRequests(){
+        server.getFriendRequests().then(result => {
+            setFriendRequests(result);
+        })
+    }
+
+    function fetchMessages(){
+
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchFriendRequests();
+    }, []))
+
     return(
         <View>
-
+            <ScrollView>
+                <Text style={styles.preferencesTitleView}>Solictudes de amistad</Text>
+                <Text style={styles.preferencesTitleView}>Mensajes</Text>
+            </ScrollView>
         </View>
     )
 }
